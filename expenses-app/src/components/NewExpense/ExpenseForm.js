9918b013ca0e1ce59ec8da84a097830dd263fb60
Andrew Morozow <andrey940@gmail.com>
2021-06-26 16:36:43 +0300

    new folder

--- expenses-app/src/components/NewExpense/ExpenseForm.js
+++ expenses-app/src/components/NewExpense/ExpenseForm.js
@@ -19,7 +19,7 @@ const ExpenseForm = (props) => {
     e.preventDefault()
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     }
 
