a0974d2e95af046ecc3029d4001a94e91f858083
Andrew Morozow <andrey940@gmail.com>
2021-06-26 20:07:38 +0300

    styles experiments

--- to-do-list/src/components/CourseGoals/CourseInput/CourseInput.js
+++ to-do-list/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+// import styled from 'styled-components'
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from '../../UI/Button/Button'
+import styles from './CourseInput.module.css'
+// import './CourseInput.css'
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? 'red' : 'black')};
+//   }
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+//   // &.invalid input {
+//   //   border-color: red;
+//   // }
+
+//   // &.invalid label {
+//   //   color: red;
+//   // }
+// `
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(true)
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true)
+    }
+    setEnteredValue(event.target.value)
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    }
+    props.onAddGoal(enteredValue)
+  }
+
+  // const dynamicColor = !isValid ? 'red' : 'black'
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* <FormControl className={!isValid && 'invalid'}> */}
+      {/* <FormControl invalid={!isValid}> */}
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
+      {/* </FormControl> */}
+      {/* </FormControl> */}
       <Button type="submit">Add Goal</Button>
     </form>
-  );
-};
+  )
+}
 
-export default CourseInput;
+export default CourseInput
