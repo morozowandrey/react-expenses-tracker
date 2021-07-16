f7246284d43ea80c792d6d809a843cf2c421672f
Andrew Morozow <andrey940@gmail.com>
2021-07-16 15:57:34 +0300

    global context wrapper

--- side-effects/src/components/Login/Login.js
+++ side-effects/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect, useReducer, useContext } from 'react'
 
 import Card from '../UI/Card/Card'
 import classes from './Login.module.css'
 import Button from '../UI/Button/Button'
+import AuthContext from '../../store/auth-context'
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -48,6 +49,8 @@ const Login = (props) => {
   // const [enteredPassword, setEnteredPassword] = useState('')
   // const [passwordIsValid, setPasswordIsValid] = useState()
   const [formIsValid, setFormIsValid] = useState(false)
+
+  const ctx = useContext(AuthContext)
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
@@ -107,7 +110,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault()
-    props.onLogin(emailState.value, passwordState.value)
+    ctx.onLogin(emailState.value, passwordState.value)
   }
 
   return (
