a3d00d7b55eb6a540946523a2352358d403c5462
Andrew Morozow <andrey940@gmail.com>
2021-07-20 12:36:40 +0300

    header btn added

--- food-order-app/src/components/Layout/Header.js
+++ food-order-app/src/components/Layout/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import HeaderCartButton from './HeaderCartButton'
+
 import mealsImage from '../../assets/meals.jpeg'
 import classes from './Header.module.css'
 
@@ -8,7 +10,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>Andrew`s Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="table full of delicious food! " />
