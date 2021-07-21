16b29e0fdbc1c112d08c318f36058474bd9ed247
Andrew Morozow <andrey940@gmail.com>
2021-07-21 19:51:07 +0300

    cart reducer logic added

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -7,9 +7,26 @@ const defaultCartState = {
 }
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item)
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    )
+    const existingCartItem = state.items[existingCartItemIndex]
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount
+
+    let updatedItems
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      }
+
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem
+    } else {
+      updatedItems = state.items.concat(action.item)
+    }
 
     return {
       items: updatedItems,
