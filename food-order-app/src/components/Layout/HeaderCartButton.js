5b5f9e0f26be3ea85ca0de221d6d5dc2bbe417ab
Andrew Morozow <andrey940@gmail.com>
2021-07-21 20:15:01 +0300

    clear setTimeout in cart

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartContext from '../../store/cart-context'
 
 import mealsImage from '../../assets/meals.jpeg'
@@ -6,13 +6,32 @@ import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
   const cartCtx = useContext(CartContext)
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const { items } = cartCtx
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount
   }, 0)
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHighlighted(true)
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
