668c90bc011aaa3d08797e5515380d2910299a0b
Andrew Morozow <andrey940@gmail.com>
2021-08-10 11:29:32 +0300

    basic store init

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,15 +1,31 @@
-import classes from './Counter.module.css';
+import classes from './Counter.module.css'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch()
+  const counter = useSelector((state) => state.counter)
+
+  const toggleCounterHandler = () => {}
+
+  const incrementCounterHandler = () => {
+    dispatch({ type: 'increment' })
+  }
+
+  const decrementCounterHandler = () => {
+    dispatch({ type: 'decrement' })
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementCounterHandler}>Increment</button>
+        <button onClick={decrementCounterHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
-  );
-};
+  )
+}
 
-export default Counter;
+export default Counter
--- redux/src/index.js
+++ redux/src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './store/store'
+
+ReactDOM.render(
+  <Provider store={store}>
+    {' '}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
