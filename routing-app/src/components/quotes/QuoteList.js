8a6db9532295e1bd6418e5d60133f1f26a20ff59
Andrew Morozow <andrey940@gmail.com>
2021-09-07 14:20:54 +0300

    quotes sorting with query parametrs

--- routing-app/src/components/quotes/QuoteList.js
+++ routing-app/src/components/quotes/QuoteList.js
@@ -1,13 +1,41 @@
-import { Fragment } from 'react';
+import { Fragment } from 'react'
+import { useHistory, useLocation } from 'react-router'
+import QuoteItem from './QuoteItem'
+import classes from './QuoteList.module.css'
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1
+    }
+  })
+}
 
 const QuoteList = (props) => {
+  const history = useHistory()
+  const location = useLocation()
+
+  const queryParams = new URLSearchParams(location.search)
+  const isSortingAscending = queryParams.get('sort') === 'asc'
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'))
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler} className="">
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+        {/* <button className=""></button> */}
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
@@ -17,7 +45,7 @@ const QuoteList = (props) => {
         ))}
       </ul>
     </Fragment>
-  );
-};
+  )
+}
 
-export default QuoteList;
+export default QuoteList
