15e52120cf249b96da134b7352d282c134721603
Andrew Morozow <andrey940@gmail.com>
2021-09-09 12:20:39 +0300

    lazy loading added

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -1,30 +1,41 @@
 import { Route, Switch, Redirect } from 'react-router'
-import NewQuote from './components/pages/NewQuote'
 import AllQuotes from './components/pages/AllQuotes'
-import QuoteDetails from './components/pages/QuoteDetails'
 import Layout from './components/layout/Layout'
-import NotFound from './components/pages/NotFound'
+import React, { Suspense } from 'react'
+import LoadingSpinner from './components/UI/LoadingSpinner'
+
+const NewQuote = React.lazy(() => import('./components/pages/NewQuote'))
+const QuoteDetails = React.lazy(() => import('./components/pages/QuoteDetails'))
+const NotFound = React.lazy(() => import('./components/pages/NotFound'))
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetails />
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetails />
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   )
 }
